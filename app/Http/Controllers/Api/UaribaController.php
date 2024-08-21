5c9925690f726e38c2dc6b671b6933f1fe7c6c8b
vresende <vini_handball@hotmail.com>
2024-08-21 17:58:01 -0300

    não verificando ssl

--- app/Http/Controllers/Api/UaribaController.php
+++ app/Http/Controllers/Api/UaribaController.php
@@ -37,7 +37,7 @@ class UaribaController extends Controller
                 'catalog_file',
                 file_get_contents($fileFullPath),
                 $request->file('catalog_file')?->getClientOriginalName()
-            )->post('https://integracao.grupomater.com.br:883/U_ARIBAADV.AP');
+            )->withoutVerifying()->post('https://integracao.grupomater.com.br:883/U_ARIBAADV.AP');
             unset($fileFullPath);
             // Verifica o status da resposta
             if ($response->successful()) {
